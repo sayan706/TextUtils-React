8197daf033249f017775c8920156fdb795324baa
sayan706 <sayanchatterjee706@gmail.com>
2023-01-23 22:22:40 +0530

    all changes done by sayan

--- src/components/About.js
+++ src/components/About.js
@@ -1,58 +1,67 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-export default function About() {
+export default function About(props){
 
 
-    const [myStyle, setMyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white',
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white',
         
-    })
-    const [btnnext, setbtnnext] = useState("Enable Dark Mode")
-    const toggleStyle = () =>{
-        if (myStyle.color === 'black') {
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '1px solid white'
+    // })
+    let myStyle = {
+        color : props.mode === 'dark'?'white':'#042743',
+        backgroundColor : props.mode === 'dark'?'#042743':'white',
+        // border: '2px solid', 
+        bordercolor: props.mode === 'dark'?'rgb{36 74 104}':'#042743'
+    }
 
-            })
-            setbtnnext("Enable Light Mode");
-        }
-        else{
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white'
+    // const [btnnext, setbtnnext] = useState("Enable Dark Mode")
+    // const toggleStyle = () =>{
+    //     if (myStyle.color === 'black') {
+    //         setMyStyle({
+    //             color: 'white',
+    //             backgroundColor: 'black',
+    //             border: '1px solid white'
 
-            })
-            setbtnnext("Enable Dark Mode");
+    //         })
+    //         setbtnnext("Enable Light Mode");
+    //     }
+    //     else{
+    //         setMyStyle({
+    //             color: 'black',
+    //             backgroundColor: 'white'
+
+    //         })
+    //         setbtnnext("Enable Dark Mode");
             
-        }
+    //     }
 
-        }
+    //     }
   return (
+    <>
       <div>
-        <div className="container" style={myStyle}>
-           <h1 className='my-3'>About Us</h1>
-          <div className="accordion" id="accordionExample">
-              <div className="accordion-item">
-                  <h2 className="accordion-header" id="headingOne">
-                      <button className="accordion-button" style= {myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                          About Us
-                      </button>
-                  </h2>
-                  <div id="collapseOne" className="accordion-collapse collapse show" style= {myStyle} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                      <div className="accordion-body" style= {myStyle}>
-                          <strong>This is a Text utillity based App.</strong> TextUtils is simply a set of utility functions to do operations on String objects. I usually have gone through many projects & I have seen many times in the code there is no use of TextUtils, I am not saying that it's necessary to use it but if we can reduce some extra work so then why not?
-                      </div>
-                  </div>
-              </div>
-          </div>
-        
-        <div className="container my-3">
+        <div className="container" style={{color : props.mode === 'dark'?'white':'#042743'}}>
+            <h1 className='my-3'>About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            About Us
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" style={myStyle} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>This is a Text utillity based App.</strong> TextUtils is simply a set of utility functions to do operations on String objects. I usually have gone through many projects & I have seen many times in the code there is no use of TextUtils, I am not saying that it's necessary to use it but if we can reduce some extra work so then why not?
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className="container my-3">
             <button onClick={toggleStyle} type="button" className="btn btn-primary">{btnnext}</button>
+        </div> */}
         </div>
         </div>
-    </div>
+    </>
   )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,18 +39,18 @@ export default function TextForm(props) {
       <div className="container" style={{color : props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="9"></textarea>
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode==='dark'?'#13466e':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="9"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClClick}>Clear Text</button>
-        <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy To Clipboard</button>
-        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleClClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy To Clipboard</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
       </div>
       <div className="container my-3" style={{color : props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length } Minutes read</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length } Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
       </div>
